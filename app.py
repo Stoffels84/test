5993e42b03628c7bcaef9150d3babcf63ee68863
Stoffels84 <info@solutionsforyou.be>
2026-02-12 21:16:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def load_schade_bron_df() -> pd.DataFrame:
     out.attrs["missing_columns"] = missing
 
     if "Datum" in out.columns:
-    out["Datum"] = pd.to_datetime(out["Datum"], errors="coerce").dt.date
+        out["Datum"] = pd.to_datetime(out["Datum"], errors="coerce").dt.date
 
 
     return out
